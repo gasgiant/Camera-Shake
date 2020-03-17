c65ebc27cca8d9ea12ac7bf1bd43a94f74a6319f
Ivan Pensionerov <31007222+gasgiant@users.noreply.github.com>
2020-03-17 23:48:28 +0300

    Explosion example

---
+++ Assets/CameraShake/Examples/Explosion/Scripts/BoxesSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoxesSpawner : MonoBehaviour
+{
+    public int num;
+    public float maxSize;
+    public float minSize;
+    public GameObject boxPrefab;
+    public List<Color> colors;
+
+    private void Start()
+    {
+        for (int i = 0; i < num; i++)
+        {
+            var box = Instantiate(boxPrefab, Random.insideUnitCircle * 5, Quaternion.identity);
+            box.GetComponent<SpriteRenderer>().color = colors[Random.Range(0, colors.Count)];
+            box.transform.localScale = Vector3.one * Random.Range(minSize, maxSize);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/CameraShake/Examples/Explosion/Scripts/ExampleController2d.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// Don't forget to add this
+using CameraShake;
+
+public class ExampleController2d : MonoBehaviour
+{
+    // Parameters of the shake to tweak in the inspector
+    public PerlinShake.Params shakeParams;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FindObjectOfType<Explosion>().Explode();
+
+            // Making camera shake
+            CameraShaker.Shake(new PerlinShake(shakeParams));
+        }
+    }
+}
